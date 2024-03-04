e38c7592f85daa80ca9522316d2259ada52feac5
Lexarlab <alexiabezerra.c@gmail.com>
2024-03-03 23:09:22 -0300

    Alterando ExercícioFila

--- src/exerciciosCollections/ExercicioFila.java
+++ src/exerciciosCollections/ExercicioFila.java
@@ -58,55 +58,6 @@ public class ExercicioFila {
 			
 		read.close();
 		
-		/*Queue<String> filaClientes = new LinkedList<>();
-        Scanner scanner = new Scanner(System.in);
-        int opcao;
-
-        do {
-            System.out.println("\nMenu:");
-            System.out.println("1: Adicionar um novo Cliente na fila");
-            System.out.println("2: Listar todos os Clientes na fila");
-            System.out.println("3: Chamar (retirar) uma pessoa da fila");
-            System.out.println("0: Sair do programa");
-            System.out.print("Digite a opção desejada: ");
-            opcao = scanner.nextInt();
-            scanner.nextLine(); // Consumir a quebra de linha
-
-            switch (opcao) {
-                case 1:
-                    System.out.print("Digite o nome do Cliente: ");
-                    String nomeCliente = scanner.nextLine();
-                    filaClientes.add(nomeCliente);
-                    System.out.println("Cliente adicionado à fila.");
-                    break;
-                case 2:
-                    if (filaClientes.isEmpty()) {
-                        System.out.println("A fila está vazia.");
-                    } else {
-                        System.out.println("Clientes na fila:");
-                        for (String cliente : filaClientes) {
-                            System.out.println(cliente);
-                        }
-                    }
-                    break;
-                case 3:
-                    if (filaClientes.isEmpty()) {
-                        System.out.println("A fila está vazia.");
-                    } else {
-                        String clienteChamado = filaClientes.poll();
-                        System.out.println("Cliente chamado: " + clienteChamado);
-                    }
-                    break;
-                case 0:
-                    System.out.println("Programa finalizado.");
-                    break;
-                default:
-                    System.out.println("Opção inválida. Tente novamente.");
-            }
-        } while (opcao != 0);
-
-        scanner.close();*/
-		
 	}
 
 }
